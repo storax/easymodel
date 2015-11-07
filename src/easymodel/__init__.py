aa6998a530e3ff7fdfe889966e72a20e01264bff
David Zuber <zuber.david@gmx.de>
2015-11-08 00:06:38 +0100

    add cascade

--- src/easymodel/__init__.py
+++ src/easymodel/__init__.py
@@ -1,3 +1,13 @@
+from __future__ import absolute_import
+
+from .treemodel import *
+from .cascade import *
+from .widgetdelegate import *
+
+__all__ = [treemodel.__all__ +
+           cascade.__all__ +
+           widgetdelegate.__all__]
+
 __author__ = 'David Zuber'
 __email__ = 'zuber.david@gmx.de'
 __version__ = '0.4.1'
